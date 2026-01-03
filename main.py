e5ea4966bc12634c785fe1e07322102e698f9ea0
levisstrauss <levisstrauss11@yahoo.com>
2026-01-02 22:14:10 -0500

    Cosmetic change

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from omegaconf import DictConfig, OmegaConf
 def go(config: DictConfig):
 
     # Setup the wandb experiment. All runs will be grouped under this name
+    # This set the project and take all the values  from the configuration file config.yaml
     os.environ["WANDB_PROJECT"] = config["main"]["project_name"]
     os.environ["WANDB_RUN_GROUP"] = config["main"]["experiment_name"]
 
@@ -25,7 +26,6 @@ def go(config: DictConfig):
 
     # Download step
     if "download" in steps_to_execute:
-
         _ = mlflow.run(
             os.path.join(root_path, "download"),
             "main",
@@ -38,36 +38,84 @@ def go(config: DictConfig):
         )
 
     if "preprocess" in steps_to_execute:
+        _ = mlflow.run(
+            os.path.join(root_path, "preprocess"),
+            "main",
+            parameters={
+                "input_artifact": "raw_data.parquet:latest",
+                "artifact_name": "preprocessed_data.csv",
+                "artifact_type": "preprocessed_data.csv",
+                "artifact_description": "Data with preprocessing applied"
+            },
+        )
 
-        ## YOUR CODE HERE: call the preprocess step
-        pass
-
+    # This is where the TEST are happening
     if "check_data" in steps_to_execute:
+        _ = mlflow.run(
+            os.path.join(root_path, "check_data"),
+            "main",
+            # This is conducting deterministic and non-deterministic TEST on the data
+            parameters={
+                "reference_artifact": config["data"]["reference_dataset"],
+                "sample_artifact": "preprocessed_data.csv:latest", # The output of the previous one
+                "ks_alpha": config["data"]["ks_alpha"]
+            },
+        )
 
-        ## YOUR CODE HERE: call the check_data step
-        pass
-
+    # SPLIT the data into train/test
     if "segregate" in steps_to_execute:
-
-        ## YOUR CODE HERE: call the segregate step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "segregate"),
+            "main",
+            parameters={
+                "input_artifact": "preprocessed_data.csv:latest", # The output of the previous component
+                # Because this is producing two data train/test: we use artifact root as single name
+                # and under that will be data/train/test
+                "artifact_root": "data",
+                "artifact_type": "segregated_data",
+                "test_size": config["data"]["test_size"], # Test size coming from the config
+                "stratify": config["data"]["stratify"]    # The stratify column
+            },
+        )
 
     if "random_forest" in steps_to_execute:
 
         # Serialize decision tree configuration
+        # Since the random forest params are too much, we are copying all of them and passing them in this file
+        # random_forest_config.yml
         model_config = os.path.abspath("random_forest_config.yml")
 
+        # This is where we go over the config containing all the params and read all of them
         with open(model_config, "w+") as fp:
             fp.write(OmegaConf.to_yaml(config["random_forest_pipeline"]))
 
-        ## YOUR CODE HERE: call the random_forest step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "random_forest"),
+            "main",
+            parameters={
+                "train_data": "data_train.csv:latest", # Previous step
+                "model_config": model_config,
+                "export_artifact": config["random_forest_pipeline"]["export_artifact"],
+                "random_seed": config["main"]["random_seed"],
+                "val_size": config["data"]["test_size"],
+                "stratify": config["data"]["stratify"]
+            },
+        )
 
     if "evaluate" in steps_to_execute:
 
-        ## YOUR CODE HERE: call the evaluate step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "evaluate"),
+            "main",
+            parameters={
+                "model_export": f"{config['random_forest_pipeline']['export_artifact']}:latest",
+                "test_data": "data_test.csv:latest"
+            },
+        )
 
 
 if __name__ == "__main__":
     go()
+
+
+# THE COMPLETE PIPELINE
